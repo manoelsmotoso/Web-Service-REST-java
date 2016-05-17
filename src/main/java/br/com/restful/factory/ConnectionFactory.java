ee49fe6b22078ba6c3b96e61ff3321d5baccf977
Manoel silva Motoso <manoelmotoso@hotmail.com>
2016-05-16 23:49:12 -0300

    restaurado

---
+++ src/main/java/br/com/restful/factory/ConnectionFactory.java
@@ -0,0 +1,81 @@
+package br.com.restful.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+/**
+ * 
+ * Classe responsavel por conter os metodos criar e fechar o banco de dados.
+ *
+ * @author Douglas Costa <douglas.cst90@gmail.com>
+ * @since 17/02/2013 01:54:02
+ * @version 1.0
+ */
+public class ConnectionFactory {
+/*
+ * 	
+ */
+/*
+	//conexão local
+ 	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/cliente";
+	private static final String USUARIO = "root";
+	private static final String SENHA = "451192";
+*/
+	//conexao openshift
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.6.238.2:3306/cliente";
+	private static final String USUARIO = "adminsHDS63j";
+	private static final String SENHA = "AbL_b2eyBwsm";
+
+
+	/**
+	 * 
+	 * Metodo responsavel por criar uma conexao com o banco 
+	 *
+	 * @return
+	 * @author Douglas Costa <douglas.cst90@gmail.com.br>
+	 * @since 17/02/2013 01:51:54
+	 * @version 1.0
+	 */
+	public Connection criarConexao(){
+		
+		Connection conexao = null;
+		
+		try {
+			
+			Class.forName(DRIVER);
+			conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+			System.out.println("Conex�o criada");
+			
+		} catch (Exception e) {
+			System.out.println("Erro ao criar conexao com o banco: " + URL);
+			e.printStackTrace();
+		}
+		return conexao;
+	}
+	
+	
+	public void fecharConexao(Connection conexao, PreparedStatement pstmt, ResultSet rs){
+		
+		try {
+			
+			if(conexao != null){
+				conexao.close();
+			}
+			if(pstmt != null){
+				pstmt.close();
+			}
+			if(rs != null){
+				rs.close();
+			}
+			System.out.println("Conex�o fechada");		
+		} catch (Exception e) {
+			System.out.println("Erro ao fechar conexao com o banco: " + URL);
+		}
+	}
+}
+
---
+++ src/main/java/br/com/restful/resource/ClienteResource.java
@@ -0,0 +1,82 @@
+package br.com.restful.resource;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import br.com.restful.controller.ClienteController;
+import br.com.restful.model.Cliente;
+
+/**
+ * Classe responsavel por conter os metodos REST de acesso ao webservice
+ * 
+ * @author motos
+ *
+ */
+@Path("/cliente")
+public class ClienteResource {
+
+	/**
+	*  Metodo responsavel por fazer chamada ao controller
+	* @return ArrayList<Cliente>
+	* @author Manoel Silva Motoso <manoelmotoso@hotmail.com>
+	* @since 12/05/2016 12:05:44
+	* @version 1.0
+	*/
+	@GET
+	@Path("/listarTodos")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Cliente> listarTodos(){
+		System.out.println("Clientes encontrados no banco");
+		return new ClienteController().listarTodos();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/id/{id}")
+	public Response getById(@PathParam("id") Long id) {
+		Cliente cliente = new ClienteController().buscarPorId(id);
+		if(cliente != null){
+				return Response
+						.ok()	
+						.type(MediaType.APPLICATION_JSON)
+						.entity(cliente)
+						.build();
+		}else{
+		return Response
+				.status(404)
+				.entity("Cliente nao encontrado")
+				.build();
+		}
+	}
+	
+
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/salvar")
+	public Response salvarClienteJson(Cliente cliente){
+		boolean isClienteGravado = new ClienteController().gravarCliente(cliente);
+		if(isClienteGravado == true){		
+		return Response
+						.ok()
+						.entity(cliente)
+						.build();
+		}else{
+			return Response
+					.status(500)
+					.entity("Erro no servidor  ao gravar cliente")
+					.build();
+		}
+		
+	}
+
+}
