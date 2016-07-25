6df0f232631ed07b462ba06b08ef7407fd219481
Manoel S. Motoso <manooel.s.motoso@gmail.com>
2016-07-25 20:04:37 -0300

    Pegando variavel de ambiente(env) da url do mysql.

--- src/main/java/br/com/restful/factory/ConnectionFactory.java
+++ src/main/java/br/com/restful/factory/ConnectionFactory.java
@@ -17,7 +17,16 @@ import java.sql.ResultSet;
 public class ConnectionFactory {
 /*
  * 	
- */
+ * 
+ 
+	
+	private static final String SENHA_MYSQL= System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+	private static final String HOST_MYSQL= System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+	private static final String PORTA_MYSQL= System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+	private static final String USUARIO_MYSQL= System.getenv("OPENSHIFT_MYSQL_DB_USER");
+	*/
+	private static final String URL_MYSQL= System.getenv("OPENSHIFT_MYSQL_DB_URL");
+	
 /*
 	//conexão local
  	private static final String DRIVER = "com.mysql.jdbc.Driver";
@@ -48,7 +57,7 @@ public class ConnectionFactory {
 		try {
 			
 			Class.forName(DRIVER);
-			conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+			conexao = DriverManager.getConnection(URL_MYSQL, USUARIO, SENHA);
 			System.out.println("Conex�o criada");
 			
 		} catch (Exception e) {
