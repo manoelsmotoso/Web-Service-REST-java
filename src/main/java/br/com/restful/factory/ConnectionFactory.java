1250e6e630962aebd834e00478501ef18c7b2a36
Manoel S. Motoso <manooel.s.motoso@gmail.com>
2016-07-25 21:05:32 -0300

    removidos arquivos sem importancia

--- src/main/java/br/com/restful/factory/ConnectionFactory.java
+++ src/main/java/br/com/restful/factory/ConnectionFactory.java
@@ -4,42 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
 /**
  * 
  * Classe responsavel por conter os metodos criar e fechar o banco de dados.
  *
- * @author Douglas Costa <douglas.cst90@gmail.com>
- * @since 17/02/2013 01:54:02
- * @version 1.0
+ * @author Manoel Silva Motoso
+ * @since 26/06/2016 01:51:54
  */
 public class ConnectionFactory {
-	/*
-	 * 	
-	 */
 	private String SENHA_MYSQL;
 	private String HOST_MYSQL;
 	private String URL_MYSQL;
 	private String PORTA_MYSQL;
 	private String USUARIO_MYSQL;
 	private String DRIVER;
-
-	/*
-	 * //conexao openshift private static final String DRIVER =
-	 * "com.mysql.jdbc.Driver"; private static final String URL =
-	 * "jdbc:mysql://127.6.238.2:3306/cliente"; private static final String
-	 * USUARIO = "adminsHDS63j"; private static final String SENHA =
-	 * "AbL_b2eyBwsm";
-	 */
-
 	/**
 	 * 
 	 * Metodo responsavel por criar uma conexao com o banco
 	 *
-	 * @return
-	 * @author Douglas Costa <douglas.cst90@gmail.com.br>
-	 * @since 17/02/2013 01:51:54
-	 * @version 1.0
+	 * @author Manoel Silva Motoso
+	 * @since 26/06/2016 01:51:54
 	 */
 	public Connection criarConexao() {
 		if (System.getenv("OPENSHIFT_MYSQL_DB_HOST") != null) {
@@ -54,6 +38,7 @@ public class ConnectionFactory {
 			USUARIO_MYSQL = "root";
 			SENHA_MYSQL = "";
 		}
+		// Montando url
 		URL_MYSQL = "jdbc:mysql://" + HOST_MYSQL + ":" + PORTA_MYSQL + "/cliente";
 		DRIVER = "com.mysql.jdbc.Driver";
 		Connection conexao = null;
@@ -84,7 +69,7 @@ public class ConnectionFactory {
 			if (rs != null) {
 				rs.close();
 			}
-			System.out.println("Conex�o fechada");
+			System.out.println("Conexao fechada");
 		} catch (Exception e) {
 			System.out.println("Erro ao fechar conexao com o banco: " + URL_MYSQL);
 		}
